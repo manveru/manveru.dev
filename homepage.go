39ac3a24df7be5cab4d4ec803c65080d1fd60e51
Michael Fellinger <m.fellinger@gmail.com>
2012-04-25 09:34:32 +0200

    a bit of source reorganization

--- homepage.go
+++ homepage.go
@@ -1,47 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"code.google.com/p/gorilla/mux" // the 800 pound gorilla
-	"errors"
 	"flag"
 	"fmt"
-	"github.com/russross/blackfriday" // markdown
-	"html/template"
-	"io"
 	"net/http"
-	"os"
-	"path/filepath"
-	"regexp"
-	"sort"
-	"strings"
-	"time"
 )
 
 var (
 	flagHost   = flag.String("host", "", "the host to listen on")
 	flagPort   = flag.String("port", "8000", "the port to listen on")
 	flagPublic = flag.String("public", "./public", "serve static files from this directory")
-
-	matchBlogHeader = regexp.MustCompile(`^#\s+(\d+-\d+-\d+):\s+(.*)`)
-	subBlogSlug     = regexp.MustCompile(`\W+`)
-
-	views       *template.Template
-	posts       = map[time.Time]*BlogPost{}
-	sortedPosts = BlogPosts{}
-	funcMap     = template.FuncMap{"date": formatDate, "xmlTime": xmlTime, "string": toString}
 )
-
-type BlogPost struct {
-	Slug string
-
-	Language, Title, Body string
-	Content               template.HTML
-
-	Created, Updated time.Time
-
-	Published bool
-}
 
 func main() {
 	// parse command line options
@@ -50,146 +20,6 @@ func main() {
 	go mapBlogPosts()
 
 	setupServer()
-}
-
-func formatDate(d time.Time) string   { return d.Format("2006-01-02") }
-func xmlTime(d time.Time) string      { return d.Format("2006-01-03T00:04:05+01:00") }
-func toString(t template.HTML) string { return string(t) }
-
-func mapBlogPosts() {
-	for {
-		var err error
-
-		views, err = template.New("views").Funcs(funcMap).ParseGlob("views/*.html")
-		if err == nil {
-			views, err = views.ParseGlob("views/*.xml")
-		}
-		if err != nil {
-			fmt.Println("error parsing templates:", err)
-			fmt.Println("Skipped update:", <-time.After(60*time.Second))
-			continue
-		}
-
-		filepath.Walk("posts", func(path string, info os.FileInfo, err error) error {
-			if err == nil {
-				if filepath.Ext(path) == ".md" {
-					return loadBlogPosts(path, info)
-				}
-			}
-			return nil
-		})
-
-		sortedPosts = BlogPosts{}
-
-		for _, post := range posts {
-			sortedPosts = append(sortedPosts, post)
-		}
-
-		sort.Sort(sortedPosts)
-
-		fmt.Println("Updating homepage:", <-time.After(60*time.Second))
-	}
-}
-
-type BlogPosts []*BlogPost
-
-func (bp BlogPosts) Len() int           { return len(bp) }
-func (bp BlogPosts) Swap(a, b int)      { bp[a], bp[b] = bp[b], bp[a] }
-func (bp BlogPosts) Less(a, b int) bool { return bp[a].Created.After(bp[b].Created) }
-
-func sortedPostsN(limit int) BlogPosts {
-	if limit > len(sortedPosts) {
-		limit = len(sortedPosts)
-	}
-
-	return sortedPosts[:limit]
-}
-
-func postById(id string) *BlogPost {
-	postCreated, err := time.Parse("2006-01-02", id)
-	if err != nil {
-		panic(err)
-	}
-
-	return posts[postCreated]
-}
-
-func loadBlogPosts(path string, info os.FileInfo) (err error) {
-	tmpPosts := []*BlogPost{}
-	lines := []string{}
-	var post *BlogPost
-
-	eachLine(path, func(line string, err error) {
-		matches := matchBlogHeader.FindStringSubmatch(line)
-		if len(matches) != 3 {
-			lines = append(lines, line)
-			return
-		}
-
-		if post != nil {
-			post.Body = strings.Join(lines, "\n")
-			post.Content = template.HTML(blackfriday.MarkdownCommon([]byte(post.Body)))
-			tmpPosts = append(tmpPosts, post)
-			lines = lines[:0]
-		}
-
-		created, err := time.Parse("2006-01-02", matches[1])
-		if err != nil {
-			return
-		}
-
-		post = &BlogPost{
-			Created:   created,
-			Updated:   info.ModTime(),
-			Published: true,
-			Language:  "en",
-			Title:     matches[2],
-			Slug:      strings.Trim(subBlogSlug.ReplaceAllString(matches[2], "-"), "-"),
-			Body:      "",
-		}
-	})
-
-	if post != nil {
-		post.Body = strings.Join(lines, "\n")
-		post.Content = template.HTML(blackfriday.MarkdownCommon([]byte(post.Body)))
-		tmpPosts = append(tmpPosts, post)
-	}
-
-	for _, post := range tmpPosts {
-		posts[post.Created] = post
-	}
-
-	return
-}
-
-func eachLine(path string, f func(line string, err error)) {
-	fd, err := os.Open(path)
-	if err != nil {
-		fmt.Println("os.Open", path, err)
-		return
-	}
-
-	// lines longer than 10k bytes will be punished
-	fdReader := bufio.NewReaderSize(fd, 10000)
-
-	var line []byte
-	isPrefix := false
-	for {
-		line, isPrefix, err = fdReader.ReadLine()
-
-		if err != nil {
-			if err != io.EOF {
-				f("", err)
-			}
-			break
-		}
-
-		if isPrefix {
-			err = errors.New("line too long")
-		}
-
-		f(string(line), err)
-	}
 }
 
 func setupServer() {
@@ -202,6 +32,8 @@ func setupServer() {
 	r.HandleFunc("/archive", blogArchive)
 	r.HandleFunc(`/blog/show/{id:\d{4}-\d{2}-\d{2}}/{language}/{slug}`, blogShow)
 	r.HandleFunc(`/blog/feed.atom`, blogAtom)
+
+	// static files
 	for _, name := range []string{"css", "js", "img", "static", "google-code-prettify"} {
 		prefix, dirName := "/"+name+"/", *flagPublic+"/"+name
 		r.PathPrefix(prefix).Handler(http.StripPrefix(prefix, http.FileServer(http.Dir(dirName))))
@@ -222,41 +54,34 @@ func render(out http.ResponseWriter, name string, val interface{}) {
 	}
 }
 
-type rargs map[string]interface{}
-type indexArgs struct {
-	Title string
-	Posts BlogPosts
-}
+type args map[string]interface{}
 
 func notFound(out http.ResponseWriter, req *http.Request) {
-	render(out, "404", rargs{"Title": "Oops"})
+	render(out, "404", args{"Title": "Oops"})
 }
 
 func mainIndex(out http.ResponseWriter, req *http.Request) {
-	render(out, "index", indexArgs{Title: "Home", Posts: sortedPostsN(20)})
+	render(out, "index", args{"Title": "Home", "Posts": sortedPostsN(20)})
 }
 
 func mainContact(out http.ResponseWriter, req *http.Request) {
-	render(out, "contact", rargs{"Title": "Contact"})
+	render(out, "contact", args{"Title": "Contact"})
 }
 
 func blogArchive(out http.ResponseWriter, req *http.Request) {
-	render(out, "blog/archive", rargs{"Title": "Archive", "Posts": sortedPostsN(1000)})
+	render(out, "blog/archive", args{"Title": "Archive", "Posts": sortedPostsN(1000)})
 }
 
 func blogShow(out http.ResponseWriter, req *http.Request) {
 	post := postById(mux.Vars(req)["id"])
-	render(out, "blog/show", rargs{"Title": post.Title, "Post": post})
+	render(out, "blog/show", args{"Title": post.Title, "Post": post})
 }
 
 func blogAtom(out http.ResponseWriter, req *http.Request) {
 	out.Header().Set("Content-Type", "application/atom+xml")
 	posts := sortedPostsN(100)
-	render(out, "blog/atom", struct {
-		Updated time.Time
-		Posts   BlogPosts
-	}{
-		Updated: posts[0].Updated,
-		Posts:   posts,
+	render(out, "blog/atom", args{
+		"Updated": posts[0].Updated,
+		"Posts":   posts,
 	})
 }
