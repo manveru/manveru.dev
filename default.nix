cc75dda1a0d0708917def8da9793f6efb9307c3d
Michael Fellinger <michael.fellinger@iohk.io>
2019-09-04 11:52:18 +0200

    final version for euphenix

--- default.nix
+++ default.nix
@@ -1,69 +1,45 @@
 let
-  euphenixSource = import ~/github/manveru/euphenix { };
-  # euphenixSource = (import (fetchTarball {
-  #   url =
-  #     "https://github.com/manveru/euphenix/archive/eaaee37df12e8fccced3a4ac93402d6b3e5fcf54.tar.gz";
-  # }) { })
+  # euphenixSource = import ~/github/manveru/euphenix { };
+  euphenixSource = (import (fetchTarball {
+    url =
+      "https://github.com/manveru/euphenix/archive/34252d3ec764e91d8578728573fa8350ce5127fe.tar.gz";
+  }) { });
 
   euphenix = euphenixSource.extend (self: super: {
     parseMarkdown =
       super.parseMarkdown.override { flags = { prismjs = true; }; };
   });
-  inherit (euphenix.lib) take optionalString;
-  inherit (euphenix) build sortByRecent loadPosts pp;
 
-  mkRoutes = args:
-    builtins.attrValues (builtins.mapAttrs (route: value:
-      let file = builtins.toFile (builtins.baseNameOf route) value.body;
-      in euphenix.mkDerivation {
-        name = "mkRoute-${builtins.baseNameOf route}";
-        buildInputs = [ euphenix.coreutils ];
-        inherit route;
-        buildCommand = ''
-          install -m 0644 -D ${file} "$out${route}"
-        '';
-      }) args);
+  inherit (euphenix.lib) take optionalString nameValuePair hasPrefix;
+  inherit (euphenix) build sortByRecent loadPosts pp mkPostCSS cssTag;
 
-in euphenix.build {
-  rootDir = ./.;
-  layout = ./templates/layout.html;
-  favicon = ./static/img/favicon.svg;
-
-  routes = mkRoutes {
-    "/index.html".body = scopedImport {
-      title = "Home";
-      cssTag = "";
-      activeClass = expected: optionalString ("/" == expected) "active";
-      route = "/";
-      liveJS = ''<script src="/js/live.js"></script>'';
-      content = scopedImport {
-        latestPosts = take 10 (sortByRecent (loadPosts "/blog/" ./blog));
-      } ./templates/home.html;
-    } ./templates/layout.html;
-
-    "/contact/index.html" = {
-      # inputs = [(css ./css/main.css)];
-      body = scopedImport {
-        title = "Contact";
-        cssTag = "";
-        activeClass = expected:
-          optionalString ("/contact/index.html" == expected) "active";
-        route = "/contact/index.html";
-        liveJS = ''<script src="/js/live.js"></script>'';
-        content = import ./templates/contact.html;
-      } ./templates/layout.html;
-    };
-  };
-
+  withLayout = body: [ ./templates/layout.html body ];
   variables = rec {
-    liveJS = ''<script src="/js/live.js"></script>'';
-    inherit pp;
+    activeClass = route: prefix:
+      optionalString (hasPrefix prefix route) "active";
     posts = sortByRecent (loadPosts "/blog/" ./blog);
-    latestPosts = take 20 posts;
+    latestPosts = take 10 posts;
+    liveJS = ''<script src="/js/live.js"></script>'';
+    css = cssTag (mkPostCSS ./css);
   };
 
-  expensiveVariables = rec {
-    posts = sortByRecent (loadPosts "/blog/" ./blog);
-    latestPosts = take 20 posts;
+  route = template: title: {
+    template = withLayout template;
+    variables = variables // { inherit title; };
   };
-}
+
+  pageRoutes = {
+    "/index.html" = route ./templates/home.html "Home";
+    "/contact/index.html" = route ./templates/contact.html "Contact";
+    "/404/index.html" = route ./templates/404.html "Not Found";
+    "/archive/index.html" = route ./templates/archive.html "Blog Archive";
+    "/blog/index.html" = route ./templates/blog.html "Blog";
+  };
+
+  blogRoutes = __listToAttrs (map (post:
+    nameValuePair post.url {
+      template = withLayout ./templates/blog_show.html;
+      variables = variables // post // { title = post.meta.title; };
+    }) variables.posts);
+
+in euphenix.build ./. { routes = pageRoutes // blogRoutes; }
